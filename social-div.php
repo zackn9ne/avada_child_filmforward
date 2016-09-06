7f82f311e1d5272a92eb6eda5ed054bce2e09e71
zackn9ne <zackn9ne@gmail.com>
2016-09-06 15:04:56 -0600

    remove lines, pop twitter link in new window rly this time

--- social-div.php
+++ social-div.php
@@ -1,5 +1,5 @@
 <div class="social-div">
 <a href="https://twitter.com/film4ward" target="_new" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
-<a href="https://www.facebook.com/FilmForward" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
-<a href="http://plus.google.com" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+<a href="https://www.facebook.com/FilmForward" target="_new" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+<a href="http://plus.google.com" target="_new" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
 </div>
